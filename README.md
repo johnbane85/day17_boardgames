e2c7bad420066fc52a5279fbb9140e87e4c3a0e8
johnbane85 <junyu.siew@gmail.com>
2022-08-04 15:25:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+// http inputs for this project
+
+// BoardgameRestController.java
+http://localhost:8080/boardgame
+http://localhost:8080/boardgame/5
+
+// BoardgamesRestController.java
+http://localhost:8080/boardgames
+http://localhost:8080/boardgames/count
+http://localhost:8080/boardgames?offset=3&limit=10
---
+++ src/main/java/vttp2022/ssf/day17_boardgames/services/BoardgameService.java
@@ -0,0 +1,37 @@
+package vttp2022.ssf.day17_boardgames.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import vttp2022.ssf.day17_boardgames.models.Boardgame;
+import vttp2022.ssf.day17_boardgames.repositories.BoardgameRepository;
+
+@Service
+public class BoardgameService {
+
+  @Autowired
+  private BoardgameRepository boardgameRepo;
+
+  public Integer count() {
+    return boardgameRepo.count();
+  }
+
+  public List<String> keys() {
+    return boardgameRepo.keys();
+  }
+
+  public Optional<Boardgame> getBoardgameById(Integer id) {
+    return getBoardgameById(id.toString());
+  }
+
+  public Optional<Boardgame> getBoardgameById(String id) {
+    String result = boardgameRepo.get(id);
+    if (null == result)
+      return Optional.empty();
+
+    return Optional.of(Boardgame.create(result));
+  }
+}
